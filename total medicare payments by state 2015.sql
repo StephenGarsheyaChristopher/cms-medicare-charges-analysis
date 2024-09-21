ee8af5b4fa9f52fc117a5f3984087eaca469d7f4
Stephen Christopher <stephenchris424@gmail.com>
2024-09-21 21:30:47 +0100

    Delete total medicare payments by state 2015.sql

--- total medicare payments by state 2015.sql
+++
@@ -1,7 +0,0 @@
---Total Medicare Payments by State (2015)
-SELECT 
-  provider_state, 
-  SUM(average_total_payments) AS total_payments 
-FROM inpatient_charges_2015
-GROUP BY provider_state
-ORDER BY total_payments DESC;
