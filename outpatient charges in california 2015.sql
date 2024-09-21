413a0419731e2cae011c383554f25f62f92065c6
Stephen Christopher <stephenchris424@gmail.com>
2024-09-21 21:30:19 +0100

    Delete outpatient charges in california 2015.sql

--- outpatient charges in california 2015.sql
+++
@@ -1,8 +0,0 @@
--- Outpatient Charges in California (2015)
-SELECT 
-  provider_state,
-  provider_id,
-  provider_name,
-  average_total_payments
-FROM outpatient_charges_2015
-WHERE provider_state = 'CA';
